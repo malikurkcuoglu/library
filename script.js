ce7cd626b8f6b2d41bbf97273ed9e7fafe62cef8
Mehmet Ali Kürkçüoğlu <115497918+malikurkcuoglu@users.noreply.github.com>
2025-07-31 18:06:12 +0300

    Add functions to create and add a book to an array

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read, arr) {
+  const newBook = new Book(title, author, pages, read);
+  arr.push(newBook);
+}

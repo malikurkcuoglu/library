2e16dfe920f6c536f49135acc43bbed16d839aa9
Mehmet Ali Kürkçüoğlu <115497918+malikurkcuoglu@users.noreply.github.com>
2025-07-31 21:02:32 +0300

    Add books display function and style the books

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const library = document.querySelector(".library");
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -12,3 +14,42 @@ function addBookToLibrary(title, author, pages, read, arr) {
   const newBook = new Book(title, author, pages, read);
   arr.push(newBook);
 }
+
+addBookToLibrary("hobbit", "tolkien", 295, true, myLibrary);
+addBookToLibrary("hobbit", "tolkien", 295, true, myLibrary);
+addBookToLibrary("hobbit", "tolkien", 295, true, myLibrary);
+addBookToLibrary("hobbit", "tolkien", 295, true, myLibrary);
+addBookToLibrary("hobbit", "tolkien", 295, true, myLibrary);
+addBookToLibrary("hobbit", "tolkien", 295, true, myLibrary);
+addBookToLibrary("hobbit", "tolkien", 295, true, myLibrary);
+addBookToLibrary("hobbit", "tolkien", 295, true, myLibrary);
+addBookToLibrary(
+  "ssssssssssssssssssssssssssssssssssssssssssssssssss",
+  "tolkien",
+  295,
+  true,
+  myLibrary
+);
+addBookToLibrary("hobbit", "tolkien", 295, true, myLibrary);
+
+function displayLibrary(arr) {
+  arr.forEach((book) => {
+    const bookTitle = document.createElement("h2");
+    bookTitle.textContent = `${book.title}`;
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const haveRead = document.createElement("p");
+    bookAuthor.textContent = `Written by ${book.author}`;
+    bookPages.textContent = `${book.pages} pages`;
+    haveRead.textContent = `${book.read ? "Read" : "Not read yet"}`;
+    const bookCard = document.createElement("div");
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(haveRead);
+    bookCard.classList.add("book");
+    library.appendChild(bookCard);
+  });
+}
+
+displayLibrary(myLibrary);
